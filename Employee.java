15ce008c6f9fe15e3b03de175d621d1f71f3408d
jawaabi123 <kjawaharabishek@gmail.com>
2025-07-22 10:34:06 +0530

    1st commit

---
+++ Employee.java
@@ -0,0 +1,68 @@
+import java.util.*;
+class Employee 
+{
+    private int salary;
+    public String name;
+
+    public Employee(int salary,String name)
+    {
+        this.salary=salary;
+        this.name=name;
+    }
+    public static void variable(int ...a)
+    {
+        for (int j : a) 
+        {
+            System.out.println(j);
+        }
+    }
+    public static void alphabet(Scanner input)
+    {
+        System.out.println("Enter alphabet");
+        char check=input.next().trim().charAt(0);
+        if(check>='a' && check<='z')
+        {
+            System.out.println("lowercase letter");
+        }
+        else{System.out.println("uppercase letter");}
+        input.close();
+    }
+    
+    public static void main(String args[])
+    {
+        //Employee e=new Employee(23400,"ljj");
+        Scanner input=new Scanner(System.in);
+        //alphabet(input);
+        String a="A man, a plan, a canal: Panama";
+        int[] newf=new int[5];
+        variable(1,2,3);
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};//2d array
+        for(int[] row:arr)
+        {
+            for(int i:row)
+            {
+                System.out.println(i);
+            }
+        }
+        String s="qwe";
+        
+        input.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
